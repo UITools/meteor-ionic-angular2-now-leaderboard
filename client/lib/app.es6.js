190c9d62895d1d99037f82c01975fd0677bdb70e
Thomas Farla <thomas-farla@hotmail.com>
2015-07-02 17:31:00 +0200

    Add ListCtrl

--- client/lib/app.es6.js
+++ client/lib/app.es6.js
@@ -1,7 +1,7 @@
 angular.extend(window, angular2now);
 angular2now.options({ controllerAs: 'vm'})
 
-angular.module('leaderboard', ['angular-meteor']);
+angular.module('leaderboard', ['angular-meteor', 'ui.router']);
 
 @Component({selector: 'leaderboard'})
 @View({ templateUrl: 'client/app.ng.html'})
---
+++ client/scores/list/list.es6.js
@@ -0,0 +1,5 @@
+@State({name: 'scoresList', url: '/', defaultRoute: true})
+@Component('home')
+@View({templateUrl: 'client/scores/list/list.ng.html'})
+class ScoresListCtrl {}
+
